30835e41ccccc0588a55aa2e47ebbed34b2073e6
Jean Vallon <jvallon44@gmail.com>
2023-04-05 18:51:35 -0400

    refactor: rename HelloView to HomeView

--- __brick__/lib/features/hello/views/home_view.dart
+++ __brick__/lib/features/hello/views/home_view.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
+
 import '../../../l10n/l10n.dart';
 
-class HelloView extends StatelessWidget {
-  const HelloView({super.key});
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
     final l10n = context.l10n;
-    
+
     return Scaffold(
       appBar: AppBar(title: const Text('{{projectName}}')),
       body: Center(
