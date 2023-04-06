78e6c582a47dfe991c303b41b59c5940aa0f2722
Jean Vallon <jvallon44@gmail.com>
2023-04-06 12:07:06 -0400

    build: use projectName everywhere

--- __brick__/lib/app/views/app.dart
+++ __brick__/lib/app/views/app.dart
@@ -20,7 +20,7 @@ class App extends StatelessWidget {
         routeInformationParser: AppRoutes.router.routeInformationParser,
         routerDelegate: AppRoutes.router.routerDelegate,
         debugShowCheckedModeBanner: false,
-        title: 'Flutter App',
+        title: '{{projectName}}',
       ),
     );
   }
