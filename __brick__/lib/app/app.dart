35c4a0404a573e0de7243ee5916f6279d496ed04
Jean Vallon <jvallon44@gmail.com>
2023-10-30 16:58:47 +0100

    fix: add TranslationProvider for Slang

--- __brick__/lib/app/app.dart
+++ __brick__/lib/app/app.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:{{projectName}}/app/routing/app_routes.dart';
+import 'package:{{projectName}}/i18n/translations.g.dart';
 
 /// The Widget that configures the application.
 class App extends StatelessWidget {
@@ -10,21 +10,21 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ProviderScope(
-      child: MaterialApp.router(
-        theme: ThemeData(
-          appBarTheme: const AppBarTheme(color: Color(0xFF13B9FF)),
-          colorScheme: ColorScheme.fromSwatch(
-            accentColor: const Color(0xFF13B9FF),
+    return TranslationProvider(
+      child: ProviderScope(
+        child: MaterialApp.router(
+          theme: ThemeData(
+            appBarTheme: const AppBarTheme(color: Color(0xFF13B9FF)),
+            colorScheme: ColorScheme.fromSwatch(
+              accentColor: const Color(0xFF13B9FF),
+            ),
           ),
+          routeInformationProvider: AppRoutes.router.routeInformationProvider,
+          routeInformationParser: AppRoutes.router.routeInformationParser,
+          routerDelegate: AppRoutes.router.routerDelegate,
+          debugShowCheckedModeBanner: false,
+          title: '{{projectName}}',
         ),
-        routeInformationProvider: AppRoutes.router.routeInformationProvider,
-        routeInformationParser: AppRoutes.router.routeInformationParser,
-        routerDelegate: AppRoutes.router.routerDelegate,
-        debugShowCheckedModeBanner: false,
-        localizationsDelegates: AppLocalizations.localizationsDelegates,
-        supportedLocales: AppLocalizations.supportedLocales,
-        title: '{{projectName}}',
       ),
     );
   }
