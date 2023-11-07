fbdd2be0fccca82ef60c9e1789053d873138454e
Jean Vallon <jvallon44@gmail.com>
2023-11-07 14:20:35 +0100

    feat: enable dark mo de

--- __brick__/lib/app/app.dart
+++ __brick__/lib/app/app.dart
@@ -3,6 +3,9 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:{{projectName}}/app/routing/app_routes.dart';
 import 'package:{{projectName}}/i18n/translations.g.dart';
 
+/// The primary color of the application.
+const primaryColor = Color(0xFF13B9FF);
+
 /// The Widget that configures the application.
 class App extends StatelessWidget {
   /// Unique constructor for class App
@@ -13,10 +16,19 @@ class App extends StatelessWidget {
     return TranslationProvider(
       child: ProviderScope(
         child: MaterialApp.router(
+          themeMode: ThemeMode.dark, // type "light" for light mode
           theme: ThemeData(
-            appBarTheme: const AppBarTheme(color: Color(0xFF13B9FF)),
+            brightness: Brightness.light,
+            appBarTheme: const AppBarTheme(color: primaryColor),
             colorScheme: ColorScheme.fromSwatch(
-              accentColor: const Color(0xFF13B9FF),
+              accentColor: primaryColor,
+            ),
+          ),
+          darkTheme: ThemeData(
+            brightness: Brightness.dark,
+            appBarTheme: const AppBarTheme(color: primaryColor),
+            colorScheme: ColorScheme.fromSwatch(
+              accentColor: primaryColor,
             ),
           ),
           routeInformationProvider: AppRoutes.router.routeInformationProvider,
