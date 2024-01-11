c4a01dd4d2f960756995e5ce4ed771afbfe5ad89
Jean Vallon <jvallon44@gmail.com>
2024-01-11 10:58:52 +0100

    fix: correct home path in app routes

--- __brick__/lib/app/{{#navBar}}navigation{{/navBar}}/app_routes.dart
+++ __brick__/lib/app/{{#navBar}}navigation{{/navBar}}/app_routes.dart
@@ -17,13 +17,13 @@ class AppRoutes {
 
   /// GoRouter instantiation, with associated routes
   static final GoRouter router = GoRouter(
-    initialLocation: Paths.albums.path,
+    initialLocation: Paths.home.path,
     navigatorKey: _rootNavigatorKey,
     routes: [
       GoRoute(
         // If a page is invalid, go_router will suggest to go to '/'
         path: '/',
-        redirect: (context, state) => Paths.albums.path,
+        redirect: (context, state) => Paths.home.path,
       ),
       StatefulShellRoute.indexedStack(
         builder: (context, state, navigationShell) {
