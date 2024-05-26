0b8cb21136c7fd919bfa831f3a20cf50984e2d15
Jean Vallon <jvallon44@gmail.com>
2024-05-26 19:18:09 +0200

    feat: add riverpod observer to log Riverpod errors

---
+++ __brick__/lib/shared/providers/riverpod_observer.dart
@@ -0,0 +1,48 @@
+import 'package:everywear_core/common/services/log.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:{{projectName}}/shared/services/log.dart';
+
+class RiverpodObserver extends ProviderObserver {
+  @override
+  void didAddProvider(
+    ProviderBase<Object?> provider,
+    Object? value,
+    ProviderContainer container,
+  ) {
+    //logger.d('Provider $provider was added with value $value');
+  }
+
+  @override
+  void didDisposeProvider(
+    ProviderBase<Object?> provider,
+    ProviderContainer container,
+  ) {
+    //logger.d('Provider $provider was disposed');
+  }
+
+  @override
+  void didUpdateProvider(
+    ProviderBase<Object?> provider,
+    Object? previousValue,
+    Object? newValue,
+    ProviderContainer container,
+  ) {
+    //logger.d('Provider $provider updated from $previousValue to $newValue');
+  }
+
+  @override
+  void providerDidFail(
+    ProviderBase<Object?> provider,
+    Object error,
+    StackTrace stackTrace,
+    ProviderContainer container,
+  ) {
+    logger.e(
+      '''
+        ⛔ ERROR IN PROVIDER : $provider
+        💬 MESSAGE : $error
+        📜 STACKTRACE : $stackTrace
+      ''',
+    );
+  }
+}
