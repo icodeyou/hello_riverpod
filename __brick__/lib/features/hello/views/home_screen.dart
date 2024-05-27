86d6a60f83d1c297ea4c34db5d77f0500f54af4c
Jean Vallon <jvallon44@gmail.com>
2024-05-27 13:11:20 +0200

    feat: async provider to set shared preferences

--- __brick__/lib/features/hello/views/home_screen.dart
+++ __brick__/lib/features/hello/views/home_screen.dart
@@ -16,6 +16,8 @@ class HomeScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final $wordState = ref.watch($wordProvider);
+
     return Scaffold(
       appBar: AppBar(title: Text(t.common.appName)),
       floatingActionButton: FloatingActionButton(
@@ -33,9 +35,28 @@ class HomeScreen extends ConsumerWidget {
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
-            Text(title, style: const TextStyle(fontWeight: FontWeight.bold)),
+            GestureDetector(
+              child: Text(
+                title,
+                style: const TextStyle(fontWeight: FontWeight.bold),
+              ),
+              onTap: () {
+                Notif.showPopup(
+                  context: context,
+                  title: 'Title',
+                  content: 'Hello',
+                  confirmButton: 'OK',
+                  cancelButton: 'Cancel',
+                  onConfirm: () => print('Confirm'),
+                );
+              },
+            ),
             const SizedBox(height: 20),
-            Text(ref.watch($wordProvider)),
+            $wordState.when(
+              data: (word) => Text(word),
+              loading: () => const CircularProgressIndicator(),
+              error: (error, stackTrace) => const Text('Error'),
+            ),
           ],
         ),
       ),
