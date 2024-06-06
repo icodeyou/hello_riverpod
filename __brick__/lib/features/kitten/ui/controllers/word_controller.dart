29da0c23015c10c6d87b198bd5e32a5e7d9f983b
Jean Vallon <jvallon44@gmail.com>
2024-06-06 18:16:37 +0200

    feat: create kitten feature

---
+++ __brick__/lib/features/kitten/ui/controllers/word_controller.dart
@@ -0,0 +1,29 @@
+import 'dart:math';
+
+import 'package:{{projectName}}/shared/constants/shared_preferences_keys.dart';
+import 'package:{{projectName}}/shared/extensions/ref_extensions.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'word_controller.g.dart';
+
+@riverpod
+class $WordController extends _$$WordController {
+  @override
+  Future<String> build() async {
+    return _getWordFromPref();
+  }
+
+  Future<void> generateLetter() async {
+    state = const AsyncLoading();
+    final random = Random();
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    final randomLetter = letters[random.nextInt(letters.length)];
+    final newWord = _getWordFromPref() + randomLetter;
+    await ref.prefs.setString(SharedPreferencesKeys.spWord, newWord);
+    state = AsyncData(newWord);
+  }
+
+  String _getWordFromPref() {
+    return ref.prefs.getString(SharedPreferencesKeys.spWord) ?? 'Hello';
+  }
+}
