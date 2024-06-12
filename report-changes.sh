0680bd72772ff96a6f20c374820edab28eacfebc
Jean Vallon <jvallon44@gmail.com>
2024-06-12 14:53:15 +0200

    build: upgrade report-changes.sh

--- report-changes.sh
+++ report-changes.sh
@@ -15,6 +15,7 @@ git add --all
 # Get the list of modified files in the git repository
 modified_files=$(git diff --name-only --staged)
 
+modified_files=$(git diff --name-status --staged | awk '/^M/ {print $2}')
 renamed_files=$(git diff --name-status --staged | awk '/^R/ {print $3}')
 created_files=$(git diff --name-status --staged | awk '/^A/ {print $2}')
 deleted_files=$(git diff --name-status --staged | awk '/^D/ {print $2}')
@@ -45,63 +46,58 @@ for file in $modified_files; do
     target_file=${matching_files[0]}
   else
     # No matches found
-    echo "❗ File '$filename' is not found in the target directory."
-    
-    fileHasBeenFound=false
-
-    index=0
-    for renamed_file in ${renamed_files[@]}; do
-      index=$((index+1))
-      if [[ $renamed_file == $file ]]; then
-        previous_names=$(git diff --name-status --staged | awk '/^R/ {print $2}')
-        previous_name=$(echo $previous_names | cut -d ' ' -f $index)
-        echo "It has been renamed from $previous_name to $file."
-        mv "$target_directory/$previous_name" "$target_directory/$file"
-        target_file="$target_directory/$file"
-        fileHasBeenFound=true
-      fi
-    done
-    
-    if [ $fileHasBeenFound = false ]; then
-      for created_file in ${created_files[@]}; do
-        if [[ $created_file == $file ]]; then
-          echo "It has been created."
-          new_file_path="$target_directory/$file"
-          mkdir -p "$(dirname "$new_file_path")"  # Ensure the directory exists
-          touch "$new_file_path"
-          target_file="$new_file_path"
-          fileHasBeenFound=true
-        fi
-      done
-    fi
-
-    if [ $fileHasBeenFound = false ]; then
-      for deleted_file in ${deleted_files[@]}; do
-        if [[ $deleted_file == $file ]]; then
-          echo "It has been deleted."
-          rm -rf "$target_directory/$file"
-          echo "✅ Successfully deleted file $file"
-          continue 2
-        fi
-      done
-    fi
-
-    if [ $fileHasBeenFound = false ]; then
-      echo "❌ File has not been renamed, it has not been created ... What happened ?"
-    fi
-
+    echo "❌ File '$filename' was modified but it is not found in the target directory."
   fi
 
-  # Ensure the target file exists before attempting to copy
-  if [ -f "$file" ]; then
-    # Copy the content of the modified file to the target file
-    cp "$file" "$target_file"
-  
-    # Replace 'projectlocal' with '{{projectName}}' in the target file
-    sed -i '' 's/projectlocal/{{projectName}}/g' "$target_file"
-  
-    echo "✅ Successfully pasted file $filename"
-  else
-    echo "❌ We tried to copy $file to $target_file but $file does not exist."
+  copyFile "$file" "$target_file"
+
+done
+
+index=0
+for renamed_file in ${renamed_files[@]}; do
+  index=$((index+1))
+  previous_names=$(git diff --name-status --staged | awk '/^R/ {print $2}')
+  previous_name=$(echo $previous_names | cut -d ' ' -f $index)
+  echo "It has been renamed from $previous_name to $renamed_file."
+  mv "$target_directory/$previous_name" "$target_directory/$renamed_file"
+  copyFile "$renamed_file" "$target_directory/$renamed_file"
+done
+
+for created_file in ${created_files[@]}; do
+  if [[ $created_file == $file ]]; then
+    echo "It has been created."
+    new_file_path="$target_directory/$file"
+    mkdir -p "$(dirname "$new_file_path")"  # Ensure the directory exists
+    touch "$new_file_path"
+    copyFile "$renamed_file" "$new_file_path"
   fi
 done
+
+for deleted_file in ${deleted_files[@]}; do
+  if [[ $deleted_file == $file ]]; then
+    echo "It has been deleted."
+    rm -rf "$target_directory/$file"
+    echo "✅ Successfully deleted file $file"
+  fi
+done
+
+
+
+
+copyFile() {
+  sourceFile=$1
+  targetFile=$2
+
+  # Ensure the target file exists before attempting to copy
+  if [[ -f "$sourceFile" ]] && [[ -f "$target_file" ]]; then
+    # Copy the content of the modified file to the target file
+    cp "$sourceFile" "$target_file"
+
+    # Replace 'projectlocal' with '{{projectName}}' in the target file
+    sed -i '' 's/projectlocal/{{projectName}}/g' "$target_file"
+
+    echo "✅ Successfully pasted file $filename"
+  else
+    echo "❌ We tried to copy the content of $sourceFile to $target_file but one of them does not exist."
+  fi
+}
