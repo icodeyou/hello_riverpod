482972731e17ae72efa205e1e59e4e9a60134117
Jean Vallon <jvallon44@gmail.com>
2024-06-14 16:06:14 +0200

    fix: load my aliases in report-changes

--- report-changes.sh
+++ report-changes.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Load my aliases
+source ~/.oh-my-zsh/custom/my_aliases.zsh > /dev/null 2>&1
+
 #Specify the folder where the script is located
 root_directory=$(dirname "$(realpath "$0")")
 
