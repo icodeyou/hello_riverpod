ddd784d6f06e91d5cd88ba8f24f8a8de0a1e2866
Jean Vallon <jvallon44@gmail.com>
2024-10-21 17:53:16 +0200

    refactor: use named routes with Go Router

--- __brick__/lib/app/startup/providers/app_startup_provider.dart
+++ __brick__/lib/app/startup/providers/app_startup_provider.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/widgets.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:{{projectName}}/shared/providers/shared_preferences_provider.dart';
 import 'package:{{projectName}}/shared/services/log.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'app_startup_provider.g.dart';
