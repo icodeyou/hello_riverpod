c6cdbb1acad624a8269522657375a232eae5f192
Jean Vallon <jvallon44@gmail.com>
2024-10-22 09:09:57 +0200

    refactor: new Riverpod syntax for Ref

--- __brick__/lib/app/init/shared_preferences/shared_preferences_provider.dart
+++ __brick__/lib/app/init/shared_preferences/shared_preferences_provider.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -5,5 +6,5 @@ part 'shared_preferences_provider.g.dart';
 
 /// Provider for SharedPreferences - eagerly loaded
 @Riverpod(keepAlive: true)
-Future<SharedPreferences> sharedPreferences(SharedPreferencesRef ref) =>
+Future<SharedPreferences> sharedPreferences(Ref ref) =>
     SharedPreferences.getInstance();
--- __brick__/lib/app/startup/providers/app_startup_provider.dart
+++ __brick__/lib/app/startup/providers/app_startup_provider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/widgets.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:{{projectName}}/app/init/shared_preferences/shared_preferences_provider.dart';
 import 'package:{{projectName}}/app/utils/log.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
@@ -7,7 +8,7 @@ part 'app_startup_provider.g.dart';
 
 /// Provider used for app initialization
 @Riverpod(keepAlive: true)
-Future<void> appStartup(AppStartupRef ref) async {
+Future<void> appStartup(Ref ref) async {
   await _initApp();
   await _initProviders(ref);
 }
@@ -19,7 +20,7 @@ Future<void> _initApp() async {
   };
 }
 
-Future<void> _initProviders(AppStartupRef ref) async {
+Future<void> _initProviders(Ref ref) async {
   // Declare all initial providers
   final initialProviders = [sharedPreferencesProvider];
 
