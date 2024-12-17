0b86802b19b42f065b36219fe0e44c304f6e98f8
Jean Vallon <jvallon44@gmail.com>
2024-12-17 16:12:22 +0100

    fix: await mutation call

--- __brick__/lib/shared/helpers/mutation/mutation.dart
+++ __brick__/lib/shared/helpers/mutation/mutation.dart
@@ -3,12 +3,12 @@ import 'package:flutter/material.dart';
 enum MutationState { idle, loading }
 
 Future<void> mutate(
-  VoidCallback mutation, {
+  Future<void> Function() mutation, {
   VoidCallback? onSuccess,
   VoidCallback? onError,
 }) async {
   try {
-    mutation();
+    await mutation();
     onSuccess?.call();
   } catch (e) {
     onError?.call();
