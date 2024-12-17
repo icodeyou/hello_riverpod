2d99e2a165610f93aa59ea5b7cc327e7e10c0831
Jean Vallon <jvallon44@gmail.com>
2024-12-17 21:04:34 +0100

    feat: add freezed to models and dtos

--- __brick__/lib/features/kitten/domain/models/kitten.dart
+++ __brick__/lib/features/kitten/domain/models/kitten.dart
@@ -1,9 +1,15 @@
-class Kitten {
-  Kitten({
-    required this.id,
-    required this.name,
-  });
+import 'package:freezed_annotation/freezed_annotation.dart';
 
-  final int id;
-  final String name;
+part 'kitten.freezed.dart';
+part 'kitten.g.dart';
+
+@freezed
+class Kitten with _$Kitten {
+  const factory Kitten({
+    required int id,
+    required String name,
+  }) = _Kitten;
+
+  factory Kitten.fromJson(Map<String, Object?> json)
+  => _$KittenFromJson(json);
 }
