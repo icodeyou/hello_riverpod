7140f3afe54d5a6bc5473395f05d49ce5945b865
Jean Vallon <jvallon44@gmail.com>
2025-02-03 16:44:02 +0100

    clean: remove all methods from repository except getAll()

--- feature_riverpod/__brick__/{{feature.snakeCase()}}/data/repository/x_{{feature.snakeCase()}}_repository.dart
+++ feature_riverpod/__brick__/{{feature.snakeCase()}}/data/repository/x_{{feature.snakeCase()}}_repository.dart
@@ -20,36 +20,11 @@ class X{{feature.pascalCase()}}Repository implements I{{feature.pascalCase()}}Re
   final X{{feature.pascalCase()}}Database {{feature.camelCase()}}DB;
 
   @override
-  Future<List<{{model.pascalCase()}}>> get{{model.pascalCase()}}s() async {
+  Future<List<{{model.pascalCase()}}>> getAll{{model.pascalCase()}}s() async {
     // Simulate a network GET request
     await Future.delayed(const Duration(milliseconds: 2000));
 
     final {{model.camelCase()}} = await {{feature.camelCase()}}DB.getAll();
     return {{model.camelCase()}}.map((k) => {{model.pascalCase()}}EntityMapper.fromEntity(k)).toList();
-  }
-
-  @override
-  Future<{{model.pascalCase()}}> get{{model.pascalCase()}}ById(int id) async {
-    final {{model.camelCase()}}Entity = await {{feature.camelCase()}}DB.getByID(id);
-    if ({{model.camelCase()}}Entity == null) {
-      // Create the {{model.camelCase()}} if it doesn't exist
-      final {{model.camelCase()}} = {{model.pascalCase()}}(id: 1, breed: 'Birman');
-      await registerNew{{model.pascalCase()}}({{model.camelCase()}});
-      return {{model.camelCase()}};
-    }
-    return {{model.pascalCase()}}EntityMapper.fromEntity({{model.camelCase()}}Entity);
-  }
-
-  @override
-  Future<void> registerNew{{model.pascalCase()}}({{model.pascalCase()}} {{model.camelCase()}}) async {
-    await {{feature.camelCase()}}DB.create({{model.pascalCase()}}EntityMapper.toEntity({{model.camelCase()}}));
-  }
-
-  @override
-  Future<void> save{{model.pascalCase()}}({{model.pascalCase()}} {{model.camelCase()}}) async {
-    // Simulate a network POST request
-    await Future.delayed(const Duration(milliseconds: 300));
-
-    await {{feature.camelCase()}}DB.update({{model.pascalCase()}}EntityMapper.toEntity({{model.camelCase()}}));
   }
 }
--- feature_riverpod/__brick__/{{feature.snakeCase()}}/domain/repository/i_{{feature.snakeCase()}}_repository.dart
+++ feature_riverpod/__brick__/{{feature.snakeCase()}}/domain/repository/i_{{feature.snakeCase()}}_repository.dart
@@ -1,8 +1,5 @@
 import '../models/{{model.snakeCase()}}.dart';
 
 abstract class I{{feature.pascalCase()}}Repository {
-  Future<List<{{model.pascalCase()}}>> get{{model.pascalCase()}}s();
-  Future<{{model.pascalCase()}}?> get{{model.pascalCase()}}ById(int id);
-  Future<void> registerNew{{model.pascalCase()}}({{model.pascalCase()}} {{model.camelCase()}});
-  Future<void> save{{model.pascalCase()}}({{model.pascalCase()}} {{model.camelCase()}});
+  Future<List<{{model.pascalCase()}}>> getAll{{model.pascalCase()}}s();
 }
