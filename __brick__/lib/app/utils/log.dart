772b014d8a8a7536bd09922ca88e3bc166143744
Jean Vallon <jvallon44@gmail.com>
2025-02-20 16:43:27 +0100

    feat: better logs

--- __brick__/lib/app/utils/log.dart
+++ __brick__/lib/app/utils/log.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:logger/logger.dart';
 
 /// Getter for singleton, accessible from anywhere
@@ -6,29 +8,59 @@ Logger get logger => Log();
 /// This class is used to log messages into the console.
 /// It is based on the package Logger.
 ///
-/// There are several levels of logs :
-/// - verbose
-/// - debug
-/// - info
-/// - warning
-/// - error
-/// - wtf
-/// - nothing
-///
 /// The class is a Singleton.
 class Log extends Logger {
-Log._()
-    : super(
-        printer: PrettyPrinter(
-          dateTimeFormat: DateTimeFormat.onlyTimeAndSinceStart,
-        ),
-      );
-
-  static final _instance = Log._();
-
-  /// Primary public constructor, returns instance of singleton 
-  // ignore: sort_unnamed_constructors_first, sort_constructors_first
+  /// Primary public constructor, returns instance of singleton
   factory Log() {
     return _instance;
+  }
+
+  Log._()
+      : super(
+          output: DeveloperConsoleOutput(),
+          printer: PrettyPrinter(
+            dateTimeFormat: DateTimeFormat.onlyTimeAndSinceStart,
+            stackTraceBeginIndex: 5,
+            errorMethodCount: 10,
+            lineLength: 200,
+            printEmojis: false,
+            noBoxingByDefault: true,
+            methodCount: 5,
+            levelColors: {
+              /*
+                https://github.com/flutter/flutter/issues/64491#issuecomment-1873673674
+                0:  Black,      8:  Grey
+                1:  Red,        9:  Red Ascend
+                2:  Green,      10: Green Ascend
+                3:  Yellow      11: Yellow Ascend
+                4:  Blue        12: Blue Ascend
+                5:  Purple      13: Purple Ascend
+                6:  Turquoise   14: Turquoise Ascend
+                7:  White       15: Bright White 
+              */
+              Level.trace: const AnsiColor.fg(6),
+              Level.debug: const AnsiColor.fg(8),
+              Level.info: const AnsiColor.fg(14),
+              Level.warning: const AnsiColor.fg(5),
+              Level.error: const AnsiColor.fg(9),
+              Level.fatal: const AnsiColor.fg(1),
+            },
+          ),
+        );
+
+  static final _instance = Log._();
+}
+
+/// This is where we output the logs to the console.
+class DeveloperConsoleOutput extends LogOutput {
+  @override
+  void output(OutputEvent event) {
+    final StringBuffer buffer = StringBuffer();
+    buffer.writeln();
+    event.lines.forEach(buffer.writeln);
+    log(
+      buffer.toString(),
+      name: DateFormat('MM.dd').format(DateTime.now()),
+    );
   }
 }
