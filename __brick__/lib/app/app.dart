b5b6435d1391102a31cb336937fa07cc461fe15e
Jean Vallon <jvallon44@gmail.com>
2025-02-27 19:20:25 +0100

    clean: pubspec

--- __brick__/lib/app/app.dart
+++ __brick__/lib/app/app.dart
@@ -1,4 +1,3 @@
-import 'package:bot_toast/bot_toast.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:{{projectName}}/app/i18n/translations.g.dart';
